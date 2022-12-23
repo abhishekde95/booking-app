e1941d66e8253f97656d83e0f97085d7da9d15a3
Abhishek De <abhishekde95@gmail.com>
2022-12-23 13:39:01 -0800

    Extension

--- main.go
+++ main.go
@@ -5,11 +5,20 @@ import (
 )
 
 func main() {
-	var conferenceName = "Go conference"
-	const conferenceTicktes = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go conference"
+	const conferenceTicktes int = 50
+	var remainingTickets int = 50
 
-	fmt.Println("Welcome to", conferenceName, "booking application")
-	fmt.Println("We have a total of", conferenceTicktes, "tickets and", remainingTickets, "are still available")
+	fmt.Printf("Welcome to %v booking application \n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available \n", conferenceTicktes, remainingTickets)
 	fmt.Println("Get your tickets to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets. \n", userName, userTickets)
+
 }
