92a2995744313b216319409729a53887290ab816
Abhishek De <abhishekde95@gmail.com>
2022-12-23 07:23:43 -0800

    More progress

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Print("Hello World")
+	var conferenceName = "Go conference"
+	const conferenceTicktes = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have a total of", conferenceTicktes, "tickets and", remainingTickets, "are still available")
+	fmt.Println("Get your tickets to attend")
 }
